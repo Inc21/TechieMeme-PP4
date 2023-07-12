d587b843aa9e47445ba0dfaf6e1c19b3128c1c1b
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-07-12 01:13:24 +0100

    Add heroku to allowed hosts

--- techiememe/settings.py
+++ techiememe/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'techiememe.herokuapp.com',
+    'techiememe-50a6f4eb45eb.herokuapp.com',
+    ]
 
 
 # Application definition
