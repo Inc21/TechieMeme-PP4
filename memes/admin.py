b0f2b86b3a388d00d212a75f76f034a4d6c4b125
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-07-30 18:22:37 +0100

    Add Tag model and connect it in admin.py (migrate)

--- memes/admin.py
+++ memes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Meme
+from .models import Meme, Review, Tag
 
 admin.site.register(Meme)
+admin.site.register(Review)
+admin.site.register(Tag)
--- memes/models.py
+++ memes/models.py
@@ -7,6 +7,9 @@ import uuid
 class Meme(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -30,3 +33,13 @@ class Review(models.Model):
 
     def __str__(self):
         return self.review
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
