258a56d37b3b9577a01248f99d0a3a416c3b44c3
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-07-30 14:33:52 +0100

    Create Meme model and register with admin panel

--- memes/models.py
+++ memes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Meme(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
