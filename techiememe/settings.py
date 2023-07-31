6ad247862a7488ec612034c9773ef48c166c8750
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-07-31 20:22:03 +0100

    Render default image when no meme image is provided

--- techiememe/settings.py
+++ techiememe/settings.py
@@ -131,10 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = ''
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/memes')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
