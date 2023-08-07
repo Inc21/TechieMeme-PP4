3b4f484d89410b17ce2d80808a790ea3be503ab5
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-07 14:54:26 +0100

    Change users views.py  to what Heroku recommends

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 def userProfiles(request):
     profiles = UserProfile.objects.all()
-    user = User.objects.get(username=request.user.username)
+    user = User.objects.get(last_login__isnull=False)
     last_login = user.last_login.strftime("%d/%m/%y")
     context = {'profiles': profiles, 'last_login': last_login}
     return render(request, "users/user_profiles.html", context)
