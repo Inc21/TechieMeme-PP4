8fd0f2d0c7b88fedc901fe7438ab1635f3e4966c
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-08 01:06:58 +0100

    (failed) Add user last_login field to the user card

--- users/views.py
+++ users/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 def userProfiles(request):
     profiles = UserProfile.objects.all()
-    user = User.objects.get(last_login__isnull=False)
-    last_login = user.last_login.strftime("%d/%m/%y")
-    context = {'profiles': profiles, 'last_login': last_login}
+    last_active = User.objects.all().last().last_login
+    print(last_active)
+    context = {'profiles': profiles, 'last_active': last_active}
     return render(request, "users/user_profiles.html", context)
 
 
