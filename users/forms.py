1a2b6d98edc72c12c4a77b58ecec2808de91b9b2
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-16 00:29:22 +0100

    Style user_form.html

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,22 @@ class UserForm(ModelForm):
             'email', 'bio', 'social_website', 'social_github',
             'social_linkedin', 'social_facebook', 'social_youtube'
             ]
+        labels = {
+            'user_img': 'Profile Image:',
+            'first_name': 'First Name:',
+            'last_name': 'Last Name:',
+            'location': 'Location:',
+            'email': 'Email:',
+            'bio': 'Bio:',
+            'social_website': 'Website:',
+            'social_github': 'Github:',
+            'social_linkedin': 'LinkedIn:',
+            'social_facebook': 'Facebook:',
+            'social_youtube': 'Youtube:'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
