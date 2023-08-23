13a397cce5e318eabdc5c982a46822fc56032095
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-23 02:06:12 +0100

    Add more styling to the index.html and javascrip for carousel

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,54 @@
 var cardCarousel = document.querySelector('#carouselExampleControls')
 
-if(window.matchMedia("(min-width: 768px)").matches){
+
     var carousel = new bootstrap.Carousel(cardCarousel, {
         interval: false,
+        wrap: true
       });
+    
+    
 
     var carouselWidth = $(".carousel-inner")[0].scrollWidth; // Get the width of the carousel
-    var cardWidth = $(".carousel-item").width(); // Get the width of a card
+    var cardWidth = $(".small-card").width(); // Get the width of a card
 
     var scrollPosition = 0; // Init the scroll position
+    var prevBtn = document.querySelector(".carousel-control-prev");
+
+    if (scrollPosition == 0) {
+        prevBtn.classList.add("invisible");
+    } else{
+        prevBtn.classList.remove("invisible");
+    }
+    
 
     $(".carousel-control-next").on("click", function () {
-        if (scrollPosition < (carouselWidth - (cardWidth * 4))) { //check if you can go any further
+        // if (scrollPosition < (carouselWidth - (cardWidth * 4))) { //check if you can go any further
+        var cardAmount = $(".small-card").length;
+        var prevBtn = document.querySelector(".carousel-control-prev");
+        var nextBtn = document.querySelector(".carousel-control-next");
+        console.log(cardAmount);
+        if (scrollPosition < (carouselWidth - (cardWidth * 4)) ) {
+            prevBtn.classList.remove("invisible");
+         };
+         if (scrollPosition > (carouselWidth - (cardWidth * cardAmount) + cardWidth)) { //check if you can go any further )) {
+            nextBtn.classList.add("invisible");
+         };
             scrollPosition = scrollPosition + cardWidth;  //update scroll position
             $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-        }
+        
     });
     $(".carousel-control-prev").on("click", function () {
-        if (scrollPosition > 0) { //check if you can go any further
+        // var cardAmount = $(".small-card").length;
+        var prevBtn = document.querySelector(".carousel-control-prev");
+        var nextBtn = document.querySelector(".carousel-control-next");
+        if (scrollPosition == 0 + cardWidth) { //check if you can go any further
+            prevBtn.classList.add("invisible");
+        };
+        if (scrollPosition < (carouselWidth - cardWidth)) { //check if you can go any further )) {
+            nextBtn.classList.remove("invisible");
+         };
             scrollPosition = scrollPosition - cardWidth;  //update scroll position
             $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-        }
     });
-} else {
-    $(cardCarousel).addClass("slide");
-}
+
 
