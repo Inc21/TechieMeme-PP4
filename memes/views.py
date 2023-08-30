32d7e59362f0a0e7f66c069f6416fcebe610512b
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-31 00:51:24 +0100

    Add link to profile image for better ux

--- memes/views.py
+++ memes/views.py
@@ -111,6 +111,7 @@ def uploadMeme(request):
         if form.is_valid():
             meme = form.save(commit=False)
             meme.uploader = profile
+            meme.save()
             form.save()
             messages.success(request, 'Meme uploaded successfully!')
             return redirect("memes")
