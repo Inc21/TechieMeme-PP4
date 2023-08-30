e198ad2dde334e7af5bb4efa77609c79b826c12c
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-08-30 23:10:28 +0100

    Change secret variable names for heroku

--- techiememe/settings.py
+++ techiememe/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 TEMPLATES = [
     {
@@ -167,8 +167,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TechieMeme',
         'USER': 'inc21',
-        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST"),
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': os.environ.get("HOST"),
         'PORT': '5432',
     }
 }
